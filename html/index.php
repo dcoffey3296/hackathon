6eafd72e1689b35599bd3825a67f89d1019e2f60
dcoffey3296 <danielpcoffey@gmail.com>
2013-12-03 20:25:27 -0500

    skeleton in place

---
+++ html/index.php
@@ -0,0 +1,24 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title></title>
+		
+		<!-- jQuery and jQuery UI -->
+		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+		<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js" type="text/javascript"></script>
+		<link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/themes/smoothness/jquery-ui.min.css" type="text/css"/>
+
+		<!-- Bootstrap 3 -->
+		<link href="https://netdna.bootstrapcdn.com/bootstrap/3.0.2/css/bootstrap.min.css" rel="stylesheet">
+		<script src="https://netdna.bootstrapcdn.com/bootstrap/3.0.2/js/bootstrap.min.js"></script>
+
+		<!-- Project Default -->
+		<link href="css/style.css" rel="stylesheet">
+		<script src="js/default.js"></script>
+	</head>
+	<body>
+	</body>
+</html>
